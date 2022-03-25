003c641da72660648d033c20e451c6bc36148d8d
Debashis Biswas <debashis.biswas@asu.edu>
2022-03-25 01:48:24 -0700

    basic notion connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+import { Client } from "@notionhq/client"
+
+const notion = new Client({ auth: '' })
+
+const databaseId = ''
+
+    ; (async () => {
+        const db = await notion.databases.query({
+            database_id: databaseId
+        })
+        console.log(db)
+    })()
