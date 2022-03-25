35f59dfac44b33d81dfd5c31b8e2af6ce7769059
Debashis Biswas <debashis.biswas@asu.edu>
2022-03-25 11:41:56 -0700

    verify config items on startup

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,17 @@
 // https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import 'dotenv/config'
 
+const verifyString = function (input: string | undefined): string {
+    if (!input) {
+        console.log("Incomplete config. Exiting...")
+        process.exit(1)
+    }
+    return input
+}
+
 class Config {
-    NOTION_ID = process.env.NOTION_ID
-    DATABASE_ID = process.env.DATABASE_ID
+    NOTION_ID = verifyString(process.env.NOTION_ID)
+    DATABASE_ID = verifyString(process.env.DATABASE_ID)
 }
 
 export const CONFIG = new Config()
--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,10 @@ import { CONFIG } from "./config"
 const notion = new Client({ auth: CONFIG.NOTION_ID })
 
 const main = async function () {
-    if (CONFIG.DATABASE_ID) {
-        const db = await notion.databases.query({
-            database_id: CONFIG.DATABASE_ID
-        })
-        console.log(db)
-    }
+    const db = await notion.databases.query({
+        database_id: CONFIG.DATABASE_ID
+    })
+    console.log(db)
 }
 
 main()
